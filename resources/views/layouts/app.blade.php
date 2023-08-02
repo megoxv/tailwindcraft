b2658ad5bc97da2d7dd40458223177ed21a65e98
Abdelmjid Saber <87904671+abdelmjid-saber@users.noreply.github.com>
2023-08-02 11:25:35 +0300

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,7 @@
         </style>
         @livewireStyles
         @stack('styles')
+        <!-- Clarity tracking code for https://tailwindcraft.com/ --><script>    (function(c,l,a,r,i,t,y){        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);    })(window, document, "clarity", "script", "i8ufi59x35");</script>
         {!! getSetting('header_code') !!}
     </head>
 
