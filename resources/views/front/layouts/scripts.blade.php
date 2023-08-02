5692a1eb9bb03c1af41373fa57ee00c1e6092dc4
Abdelmjid Saber <87904671+abdelmjid-saber@users.noreply.github.com>
2023-08-02 11:24:17 +0300

    Update scripts.blade.php

--- resources/views/front/layouts/scripts.blade.php
+++ resources/views/front/layouts/scripts.blade.php
@@ -13,7 +13,7 @@
     </script>
 @endif
 {{-- End Google Analytics --}}
-
+<!-- Clarity tracking code for https://tailwindcraft.com/ --><script>    (function(c,l,a,r,i,t,y){        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);    })(window, document, "clarity", "script", "i8ufi59x35");</script>
 @stack('modals')
 @stack('scripts')
 @livewireScripts
