bb0e9ae3f29dbbdf385c610e69d4ca851c25ed10
Abdelmjid Saber <87904671+abdelmjid-saber@users.noreply.github.com>
2023-08-04 22:41:13 +0300

    fix

--- resources/views/front/layouts/footer.blade.php
+++ resources/views/front/layouts/footer.blade.php
@@ -1,3 +1,5 @@
+<x-ad-component slug="sidebar" />
+
 <footer class="bg-gray-25">
     <div class="container max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex flex-wrap items-center justify-between">
